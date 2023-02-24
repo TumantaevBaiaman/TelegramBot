0733dd715fb5eebab4d415e96d87fef2aabaeaed
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-02-25 00:58:48 +0600

    fix datetime2

--- export_excel/get_data.py
+++ export_excel/get_data.py
@@ -36,7 +36,7 @@ async def try_api():
 
     delivery_url = 'https://kaspi.kz/mc/api/orderTabs/active?count=50&selectedTabs=DELIVERY&startIndex=0&returnedToWarehouse=false'
     pickup_url = 'https://kaspi.kz/mc/api/orderTabs/active?count=50&selectedTabs=PICKUP&startIndex=0&returnedToWarehouse=false'
-    archive_url = f'https://kaspi.kz/mc/api/orderTabs/archive?start=0&count=5000&fromDate={start()}&toDate={time()}&statuses=CANCELLED&statuses=COMPLETED&statuses=RETURNED&statuses=RETURN_REQUESTED&statuses=CREDIT_TERMINATION_PROCESS'
+    archive_url = f'https://kaspi.kz/mc/api/orderTabs/archive?start=0&count=250000&fromDate={start()}&toDate={time()}&statuses=CANCELLED&statuses=COMPLETED&statuses=RETURNED&statuses=RETURN_REQUESTED&statuses=CREDIT_TERMINATION_PROCESS'
 
     request = s.get(
         delivery_url,
